98c1cca1b6cc669f5962552fa07211bc3c980713
MariSlvrstv <selivetstova.m@gmail.com>
2025-03-09 16:14:09 +0300

    first-commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,11 +1,32 @@
 package actioninfo
 
 import (
-	...
+	"fmt"
 )
 
 // создайте интерфейс DataParser
-...
+
+type DataParser interface {
+	Parse(data string) error
+	ActionInfo() (string, error)
+}
 
 // создайте функцию Info()
-...
+func Info(dataset []string, dp DataParser) {
+	for _, data := range dataset {
+
+		err := dp.Parse(data)
+		if err != nil {
+			fmt.Printf("Ошибка парсинга: %v\n", err)
+			continue
+		}
+
+		info, err := dp.ActionInfo()
+		if err != nil {
+			fmt.Printf("Ошибка получения информации: %v\n", err)
+			continue
+		}
+
+		fmt.Println(info)
+	}
+}
